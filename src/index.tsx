8c1803f0f29337b03e9594981783971a436d68f3
Vitor Guilhermo <viguilhermo@hotmail.com>
2021-12-08 17:49:00 -0300

    Fazendo uso de API com mirageJS e axios

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import { createServer } from 'miragejs';
 import { App } from './App';
 
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
